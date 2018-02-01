97f033416161cffe2ecf3aab9905c528e08b49ec
panyoujin <904709108@qq.com>
2018-02-01 16:41:45 +0800

    增加demo

---
+++ DBHelper/DBDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using DBHelper.SQLHelper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DBDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var list = SQLHelperFactory.Instance.QueryForList("GetData", null);
+                if (list != null)
+                {
+                    foreach (var item in list)
+                    {
+                        Console.WriteLine(string.Format("{0}:{1}", item.ID, item.NAME));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            Console.Read();
+        }
+    }
+}
